6d49a0c0d8f88da4ea1c3b76a6c17fe970e917a1
Ilyas Esmail <ilyas@esmail.pt>
2021-05-10 10:53:02 +0100

    Save file when balance is found + log each check in the console.

--- Generate_Addresses_From_Seed.js
+++ Generate_Addresses_From_Seed.js
@@ -3,6 +3,7 @@ const Web3 = require('web3');
 const hdkey = require('ethereumjs-wallet');
 const bip39 = require('bip39');
 var api = require('etherscan-api').init('*Your Etherscan Key*');
+var fs = require('fs');
 
 async function hack () {
     const path = "m/44'/60'/0'/0/0";
@@ -19,13 +20,14 @@ async function hack () {
         wallet = hdwallet.derivePath(path).getWallet();
         address = `0x${wallet.getAddress().toString('hex')}`;
         balance = await api.account.balance(address);
-        process.stdout.cursorTo(0);
-        process.stdout.write(`Checking: ${address}`);
+        console.log(`Checking: ${address}, https://etherscan.io/address/${address}, ${mnemonicPhrase}, ${balance}`);
         if(balance > 0) {
             console.log('\n');
             console.log('mnemonic --->>' + ' '+ mnemonicPhrase)
             console.log('balance --->>' + ' '+ balance)
             console.log('\n');
+            fs.writeFile("${address}", "Found! ${address}, https://etherscan.io/address/${address}, ${mnemonicPhrase}, ${balance}");
+
         }
     }
 }
